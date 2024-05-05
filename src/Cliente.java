dd52a74ebce0e0a8c46f7460a1f27d18112fca53
Ana Julia <anajufg127@gmail.com>
2024-05-05 11:08:07 -0300

    Create Cliente.java

---
+++ src/Cliente.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class Cliente {
+    String nome;
+    String endereco;
+    Date data;
+
+    void imprimir() {
+        System.out.println("Cliente inválido");
+    }
+}
